8b796f1683ac5b87b5c8e99e4180fa96aa9cb191
thiagoarruda09 <thiago.arruda.09@hotmail.com>
2025-02-27 20:46:09 -0300

    adiciona a busca na api

--- js/eventos.js
+++ js/eventos.js
@@ -8,41 +8,46 @@ const SELECT_CIDADE = document.getElementById('cidade')
 
 function atualizarRegioes(){
 
-    let regioes = [
-        'norte',
-        'nordeste',
-        'sul',
-        'sudeste',
-        'centro oeste'
-    ]
-regioes = regioes.map(cadaRegiao=>`<option>${cadaRegiao}</option>`)
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/regioes')
+         .then(res => res.json())
+         .then(regioes =>{
+            regioes = regioes.map(cadaRegiao=>`<option value=${cadaRegiao.id}>${cadaRegiao.nome}</option>`)
 SELECT_REGIAO.innerHTML += regioes
+
+         })
+
+    
+
     // regioes.forEach(cadaRegiao => {
     //     SELECT_REGIAO.innerHTML += `<option>${cadaRegiao}</option>`
     // })
 }
 
 function atualizarEstados(){
-    let estados = [
-        'estado 1',
-        'estado 2',
-        'estado 3'
-    ]
-    estados = estados.map(cadaEstado => `<option>${cadaEstado}</option>`)
+    let id = SELECT_REGIAO.value
+ fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/regioes/${id}/estados
+`)
+.then(res => res.json())
+.then(estados =>{
+    estados = estados.map(cadaEstado => `<option value = ${cadaEstado.id}>${cadaEstado.nome}</option>`)
     SELECT_ESTADO.innerHTML += estados
+})
+    
 }
 
 function atualizarCidades(){
-    let cidades = [
-        'cidade 1',
-        'cidade 2',
-        'cidade 3'
-    ]
-
-    cidades = cidades.map(cadaCidade=> `<option>${cadaCidade}</option>`)
+    let id = SELECT_ESTADO.value
+fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${id}/municipios
+`)
+.then(res => res.json())
+.then(cidades =>{
+    cidades = cidades.map(cadaCidade=> `<option>${cadaCidade.nome}</option>`)
     SELECT_CIDADE.innerHTML += cidades
+
+})
+
+   
 }
 
 atualizarRegioes()
-atualizarEstados()
-atualizarCidades()
+
